72cfeb9873251fccc86a87a899e27cf52283c9f5
Ali Amrahli <ali.emrahli@gmail.com>
2023-11-27 17:56:42 +0400

    Fixed sending verification request part. Added handling incoming verify requests in different cases.

--- node.py
+++ node.py
@@ -8,13 +8,14 @@ class RingNode:
         self.node_id = node_id
         self.ip_address = ip_address
         self.port = port
-        self.neighbors = {}  # {node_id: ip_address}
+        self.known_node = {}  # {neighbor_ip: neighbor_port}
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((ip_address, port))
         self.successor_id = None
         self.successor_ip = None
         self.predecessor_id = None
         self.predecessor_ip = None
+        self.node_list = {"id": [], "ipADDR": []}
 
     def start(self):
         self.verify_id()
@@ -22,30 +23,56 @@ class RingNode:
     def verify_id(self):
         while True:
             # Incrementally try to get a unique ID
-            verified, self.node_id, self.successor_id, self.successor_ip = self.request_verification(self.node_id + 1)
+            verified, self.node_id, self.successor_ip, self.node_list = (
+                self.request_verification(self.node_id + 1, self.ip_address))
 
             if verified:
                 # self.node_id = next_node_id
                 # self.neighbors[next_node_id] = next_node_ip
                 print(f"Node {self.node_id} verified.")
+                self.handle_messages()
                 break
 
-    def request_verification(self, target_id):
+    def request_verification(self, target_id, target_ip):
         while True:
-            verification_message = f"VERIFY ID {target_id}"
-            for neighbor_id, neighbor_ip in self.neighbors.items():
-                self.socket.sendto(verification_message.encode(), (neighbor_ip, self.port))
+            verification_message = f"VERIFY ID {target_id} {target_ip}"
+            for neighbor_ip, neighbor_port in self.known_node.items():
+                self.socket.sendto(verification_message.encode(), (neighbor_ip, 5555))
 
-            response, _ = self.socket.recvfrom(1024)
+            response, _ = self.socket.recvfrom(1024)  # response equals only "VERIFIED" or whole message?
             response_parts = response.decode().split()
 
             if response_parts[0] == "VERIFIED":
-                # VERIFIED ID 4 3 127.43.56.21
-                return True, int(response_parts[2]), int(response_parts[3]), response_parts[4]
+                # VERIFIED ID 4 127.43.56.21 node_list{}
+                return True, int(response_parts[2]), response_parts[3], response_parts[4]
             else:
                 return False, 0, ""
 
-    # def handle_messages:
+    def handle_messages(self):
+        while True:
+            message, _ = self.socket.recvfrom(1024)
+            message_parts = message.decode().split()
+            print(message)
+            if message_parts[0] == "VERIFY":
+                target_ip = message_parts[3]
+                target_id = message_parts[2]
+                # finding index of my IP address in the list
+                index = self.node_list["ipADDR"].index(self.ip_address)
+                successor_index = index + 1
+                self.successor_ip = self.node_list["ipADDR"][successor_index]
+                verification_message = f"VERIFIED {message_parts[2]} {self.successor_ip}"
+                # below condition shows that this request came second time as it is in
+                # exists in the node_list
+                # this condition may not work in very exclusive cases; may be will be updated
+                if any(message_parts[3] in values for values in self.node_list.values()):
+                    self.socket.sendto(verification_message.encode(), (target_ip, 5555))
+                else:
+                    # forward the request to next node
+                    self.socket.sendto(message.encode(), (self.successor_ip, 5555))
+                    self.node_list["id"].append(target_id)
+                    self.node_list["ipADDR"].append(target_ip)
+
+        pass
 
 
 if __name__ == "__main__":
